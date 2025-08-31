39ee07f9b8fddf40dbad14596bde83a05b5c8ab0
Tushar <tuharbansal3366@gmail.com>
2025-08-31 15:12:23 +0530

    day-30

--- day30.cpp
+++ day30.cpp
@@ -1,9 +1,26 @@
-// Day 30 solution
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& nm) {
+        unordered_set<string>mp;
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(nm[i][j]=='.') continue;
 
-#include <iostream>
-using namespace std;
+                string r=to_string(i)+"r"+to_string(nm[i][j]);
+                string c=to_string(j)+"c"+to_string(nm[i][j]);
+                string m=to_string(i/3)+":"+to_string(j/3)+"m"+to_string(nm[i][j]);
 
-int main() {
-    // Your code here
-    return 0;
-}
+
+                if(mp.find(r)!=mp.end()||mp.find(c)!=mp.end()||mp.find(m)!=mp.end())
+                return false;
+                
+                mp.insert(r);
+                 mp.insert(c);
+                  mp.insert(m);
+
+
+            }
+        }
+        return 1;
+    }
+};
